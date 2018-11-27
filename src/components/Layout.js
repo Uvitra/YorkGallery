d63e7b396cbfa571258ae35fd9206250cbbdecee
Uvitra <Uvitra@users.noreply.github.com>
2018-11-27 11:09:53 -0500

    updated components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,11 +4,13 @@ import Helmet from 'react-helmet'
 import Navbar from '../components/Navbar'
 // import './all.sass'
 import './constants.less'
-import './styles.less'
+import './gallery-style.less'
 
 const TemplateWrapper = ({ children }) => (
 	<div>
-		<Helmet title="Design at York Gallery" />
+		<Helmet>
+			<title>Design at York Gallery</title>
+		</Helmet>
 		<Navbar />
 		<div>{children}</div>
 	</div>
