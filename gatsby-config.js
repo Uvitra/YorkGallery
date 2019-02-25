5253cd7ede4ce942d325cc4b0065d4d78047ce30
Aaron Vince <aaronvince@users.noreply.github.com>
2019-02-25 11:09:54 -0500

    got rid of purgecss
    Not necessary, also broke my className references

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,7 +105,7 @@ module.exports = {
 			}
 		},
 		'gatsby-plugin-offline',
-		'gatsby-plugin-purgecss', // must be after other CSS plugins
+		// 'gatsby-plugin-purgecss', // must be after other CSS plugins
 		'gatsby-plugin-netlify' // make sure to keep it last in the array
 	]
 }
